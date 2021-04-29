6aa6d9ef73b78effe5c68d27edde568a69ce8ec8
Mona Lisa <sam.roberson32@gmail.com>
2021-04-28 23:10:34 -0600

    adding functionality to js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+$('currentDay').txt(moment().format("dddd, MMMM Do YYYY"));
+
+$("btn").on("click", function () {
+    var id = $(this).attr("data-id");
+    var text =$("#" + id).val();
+    localStorage.setItem(id, text);
+}
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+var time = [9, 10, 11 ,12 ,13 ,14 ,15,16 ,17];
+
+for (var i =0; i < time.length; i++) {
+
+    if (currentHour < time[i]) {
+        $("#" + i).addClass("future");
+    } else if (currentHour > time[i]) {
+        $("#" + i).addClass("past");
+    }else {
+        $("#" + i).addClass("present");
+    }
+    }
